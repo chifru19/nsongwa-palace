f4163a15048e8ad018f2b044ffcebbf4a2beb92a
Frank Fru <chifru19@googlemail.com>
2026-06-16 14:33:45 +0200

    Start fresh: Initial structure for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://chifru19.github.io",
+  base: "/nsongwa-palace/",
+});
